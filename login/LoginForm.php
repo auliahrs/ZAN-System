7ac7d048dcb18414835ee438e1278d48a69a1ee4
aidlizz <izzaharuddin21@gmail.com>
2023-06-03 16:19:31 +0800

    login

--- login/LoginForm.php
+++ login/LoginForm.php
@@ -21,6 +21,12 @@
         height: 100%;
         }
         }
+        body{
+            background-image : url(../Background.png);
+            position: absolute;
+width: 1450px;
+height: 1014px;
+        }
     </style>
 </head>
 <body>
@@ -30,34 +36,30 @@
  
   <div class="form-outline mb-4">
     <input type="email" id="form2Example1" class="form-control" />
-    <label class="form-label" for="form2Example1">Email address</label>
+    <label class="form-label" for="form2Example1">No Kad Pengenalan</label>
   </div>
 
 
  
   <div class="form-outline mb-4">
     <input type="password" id="form2Example2" class="form-control" />
-    <label class="form-label" for="form2Example2">Password</label>
+    <label class="form-label" for="form2Example2">Kata Laluan</label>
   </div>
 
 
   <div class="row mb-4">
     <div class="col d-flex justify-content-center">
 
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" value="" id="form2Example34" checked />
-        <label class="form-check-label" for="form2Example34"> Remember me </label>
-      </div>
-    </div>
+      
 
-    <div class="col">
+  
+  </div>
+
+  <button type="submit" class="btn btn-primary btn-block mb-4">Masuk</button>
+  <div class="col">
   
       <a href="#!">Forgot password?</a>
     </div>
-  </div>
-
-  <button type="submit" class="btn btn-primary btn-block mb-4">Sign in</button>
-
 
   <div class="text-center">
     <p>Forgot Password <a href="#!">Register</a></p>
