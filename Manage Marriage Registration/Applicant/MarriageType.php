09ab97ee940302bb57f16787c3f6ecf353efb63b
HttRicky <cb21080@student.ump.edu.my>
2023-06-10 21:43:00 +0800

    10/6

---
+++ Manage Marriage Registration/Applicant/MarriageType.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="/ZAN-System/css/myCSS.css">
+    <title>TITLE GOES HERE</title>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <!-- HEADER BAR -->
+        <?php //include('C:/xampp/htdocs/ZAN-System/components/titleBar.php'); ?>
+        <!-- HEADER BAR ENDS -->
+        <!-- BODY -->
+        <div class="row">
+            <!-- LEFT MENU -->
+            <?php //include('C:/xampp/htdocs/ZAN-System/components/S_leftMenu.php'); ?>
+            <!-- LEFT MENU ENDS HERE -->
+
+            <!-- THE CONTENT -->
+            7\]
+            <!-- THE CONTENT ENDS HERE -->
+        </div>
+        <!-- BODY ENDS -->
+    </div>
+</body>
+
+</html>
