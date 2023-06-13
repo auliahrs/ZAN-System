f11ee6c2de097e69b99ebea43d462f1a518e8668
HttRicky <cb21080@student.ump.edu.my>
2023-06-14 00:25:23 +0800

    Update MarriageType.php

--- Manage Marriage Registration/Applicant/MarriageType.php
+++ Manage Marriage Registration/Applicant/MarriageType.php
@@ -57,7 +57,7 @@
                 <div class="row">
                     <div class="col-md-12">
                         <div class="content-body">
-                            <form action="/ZAN-System/Manage Pre-marriage Course Application/Applicant/A_DaftarPerkahwinan.php" method="post">
+                            <form action="/ZAN-System/Manage Marriage Registration/Applicant/MarriageType.php" method="post">
                                 <div class="row my-3">
                                     <div class="col table-dpp">
                                         <table>
