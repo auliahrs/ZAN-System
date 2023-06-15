cbffe1b8afb30a153e81ad3deee57f6696252e47
auliahrs <hasnhiahaulia27@gmail.com>
2023-06-16 00:59:01 +0800

    modified:   Manage Pre-marriage Course Application/application.php 	renamed:    facade/index.php -> index.php

---
+++ Manage Pre-marriage Course Application/application.php
@@ -0,0 +1,93 @@
+<?php
+    class Applicant{
+        private $noIC;
+        private $applicantNama;
+        private $applicantDOB;
+        private $applicantUmur;
+        private $applicantJantina;
+        private $applicantBangsa;
+        private $applicantKewarganegaraan;
+        private $applicantAlamat;
+        private $applicantNoTel;
+        private $applicantNoTelRumah;
+        private $applicantTarafPendidikan;
+        private $applicantSektorPekerjaan;
+        private $applicantPekerjaan;
+        private $applicantAlamatkerja;
+        private $applicantStatusKahwin;
+        private $applicantStatusSaudaraBaru;
+        private $applicantNoSijilPraPerkahwinan;
+
+        public function getNoIc(){
+            return $this->noIC;
+        }
+
+        public function getApplicantNama(){
+            return $this->applicantNama;
+        }
+
+        public function getApplicantDOB(){
+            return $this->applicantDOB;
+        }
+
+        public function getApplicantUmur(){
+            return $this->applicantUmur;
+        }
+
+        public function getApplicantJantina(){
+            return $this->applicantJantina;
+        }
+
+        public function getApplicantBangsa(){
+            return $this->applicantBangsa;
+        }
+
+        public function getApplicantKewarganegaraan(){
+            return $this->applicantKewarganegaraan;
+        }
+
+        public function getApplicantAlamat(){
+            return $this->applicantAlamat;
+        }
+
+        public function getApplicantNoTel(){
+            return $this->applicantNoTel;
+        }
+
+        public function getApplicantNoTelRumah(){
+            return $this->applicantNoTelRumah;
+        }
+
+        public function getApplicantTarafPendidikan(){
+            return $this->applicantTarafPendidikan;
+        }
+
+        public function getApplicantSektorPekerjaan(){
+            return $this->applicantSektorPekerjaan;
+        }
+
+        public function getApplicantPekerjaan(){
+            return $this->applicantPekerjaan;
+        }
+
+        public function getApplicantAlamatKerja(){
+            return $this->applicantAlamatkerja;
+        }
+
+        public function getApplicantStatusKahwin(){
+            return $this->applicantStatusKahwin;
+        }
+
+        public function getApplicantStatusSaudaraBaru(){
+            return $this->applicantStatusSaudaraBaru;
+        }
+
+        public function getApplicantNoSijilPraPerkahwinan(){
+            return $this->applicantNoSijilPraPerkahwinan;
+        }
+
+        public function retrieve(){
+            
+        }
+    }
+?> 
